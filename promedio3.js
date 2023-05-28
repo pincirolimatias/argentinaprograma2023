8b9b5b1d61b57fd8956bf7230b583754a175021f
pincirolimatias <matiaspinciroli@gmail.com>
2023-05-27 21:52:05 -0300

    practico No finalizado

---
+++ promedio3.js
@@ -0,0 +1,23 @@
+let x1 = 5
+let y1 = 7
+let z1 = 9
+let promedio1 = (x1 + y1 + z1)/3
+console.log (promedio1)
+
+// devuelve el valor de 7
+
+let x2 = -2
+let y2 = -3
+let z2 = -8
+let promedio2 = (x2 + y2 + z2)/3
+console.log (promedio2)
+
+//devuelve el valor de -4.333333333333333
+
+let x3 = -1.2
+let y3 = 5.3
+let z3 = -3.3
+let promedio3 = (x3 + y3 + z3)/3
+console.log (promedio3)
+
+//devuelve el valor de 0.2666666666666666
