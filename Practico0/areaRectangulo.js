2252c1315d66ac936259cbabe8ed8530cde716af
pincirolimatias <matiaspinciroli@gmail.com>
2023-05-30 20:57:34 -0300

    Practico No finalizado

---
+++ Practico0/areaRectangulo.js
@@ -0,0 +1,13 @@
+let base1 = 5
+let altura1 = 3
+let area1 = base1 * altura1
+console.log (area1)
+
+//devuelve el valor de 15
+
+let base2 = 12.5
+let altura2 = 5.3
+let area2 = base2 * altura2
+console.log (area2)
+
+//devuelve el valor de 66.25
---
+++ Practico0/promedio3.js
@@ -0,0 +1,23 @@
+let numero1 = 5
+let numero2 = 7
+let numero3 = 9
+let promedio1 = (numero1 + numero2 + numero3)/3
+console.log (promedio1)
+
+// devuelve un valor de 7
+
+let numero4 = -2
+let numero5 = -3
+let numero6 = -8
+let promedio2 = (numero4 + numero5 + numero6)/3
+console.log (promedio2)
+
+//devuelve un valor de -4.333333333333333
+
+let numero7 = -1.2
+let numero8 = 5.3
+let numero9 = -3.3
+let promedio3 = (numero7 + numero8 + numero9)/3
+console.log (promedio3)
+
+//devuelve un valor de 0.2666666666666666
